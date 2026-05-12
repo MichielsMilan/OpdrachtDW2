74f895004e7fbbfe5776e127845fbd268d297d84
MilanMichiels <milanmichiels2004@gmail.com>
2026-05-12 22:20:17 +0200

    Docker opdracht

--- build-en-start-op-poort-8888.sh
+++ build-en-start-op-poort-8888.sh
@@ -2,7 +2,4 @@
 
 docker build -t missing-manual-site .
 
-docker stop missing-manual-container 2>/dev/null
-docker rm missing-manual-container 2>/dev/null
-
 docker run -dt --name missing-manual-container -p 8888:80 missing-manual-site
